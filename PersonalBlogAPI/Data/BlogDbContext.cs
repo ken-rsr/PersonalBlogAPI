a1e02e1d658c5d1f26fdd37e937b49af67540a5d
ken-rsr <jonken.rosario@cvsu.edu.ph>
2025-12-09 21:49:57 +0800

    added model and DbContext

---
+++ PersonalBlogAPI/Data/BlogDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalBlogAPI.Model;
+
+namespace PersonalBlogAPI.Data
+{
+    public class BlogDbContext(DbContextOptions<BlogDbContext> options): DbContext(options)
+    {
+        public DbSet<Blog> Blogs => Set<Blog>();
+    }
+}
---
+++ PersonalBlogAPI/Model/Blog.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonalBlogAPI.Model
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+    }
+}
